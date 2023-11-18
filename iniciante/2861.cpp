80827a041f7e7b2376424ccc08405eb54c39d6a9
Maicom Ferreira <maicomferre@hotmail.com>
2023-11-17 23:02:52 -0300

    Add 'A Saída' - 2861

---
+++ iniciante/2861.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int lines;
+	string t;
+
+	cin >> lines;
+
+	cin.ignore();
+	
+	for(; lines>0; lines--)
+	{
+		getline(cin,t);
+		cout << "gzuz" << endl;
+	}
+	return 0;
+}
